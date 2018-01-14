2856a78c8440fbc64694450c0e59c7695ba3e238
Harry Sun <zoomhgtk@gmail.com>
2018-01-14 18:17:09 +0800

    changed to Generic view, the generic should be imported first

--- mysite1/polls/views.py
+++ mysite1/polls/views.py
@@ -3,25 +3,37 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
+from django.views import generic
+
 from .models import Choice, Question
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-            'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question}) 
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-#the only difference between "detail" and "results" is the template name used by "render"
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    #context = {
+    #        'latest_question_list': latest_question_list,
+    #}
+    #return HttpResponse(template.render(context, request))
+# we are changing to Generic views. A sense of Object Oriented.
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/detail.html', {'question': question}) 
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+    #question = get_object_or_404(Question, pk=question_id)
+    #return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
