e26bb2b3f8656950c296f68057a4907fad935665
Harry Sun <zoomhgtk@gmail.com>
2018-01-14 16:19:48 +0800

    updated views

--- mysite1/polls/views.py
+++ mysite1/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 # Create your views here.
 
 def index(request):
@@ -22,4 +23,12 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls.detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+            }) 
     return HttpResponse("You're voting on question %s." % question_id)
