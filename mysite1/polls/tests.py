61583ac592a43d834db40f95426e71e7c18976ee
Harry Sun <zoomhgtk@gmail.com>
2018-01-21 14:56:22 +0800

    adding a rough test fuction, and it needs to be optimized

--- mysite1/polls/tests.py
+++ mysite1/polls/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        # questions created in the future will be marked as false when asked if published recently
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
