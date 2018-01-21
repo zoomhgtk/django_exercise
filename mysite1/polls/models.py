d9b7c50ee190bc52b26aea4e70c45b663fdebe9a
Harry Sun <zoomhgtk@gmail.com>
2018-01-21 15:06:18 +0800

    new was_published_recently function under class Question

--- mysite1/polls/models.py
+++ mysite1/polls/models.py
@@ -10,7 +10,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # tutorial05: optimizing the test function
+        #return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -18,3 +21,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
